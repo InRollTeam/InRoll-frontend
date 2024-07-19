4c4c49aac7ffb9f4e5ed3b1cace87e742208e0db
regsah <123820415+regsah@users.noreply.github.com>
2024-07-19 22:42:36 +0300

    Update NavBar.js
    minor fix to the colors

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-center bg-light container-fluid">
+    <nav class="navbar navbar-expand-lg justify-content-center bg-light container-fluid">
       <div class="container-xl row justify-content-between">
           <Link to="/jobs" class="nav-link col-3 col-md-2 p-1 fs-5 fw-bolder text-center">Job Board</Link>
           <Link to="/" class="nav-link col-3 col-md-2 p-1 fs-4 fw-bold text-center">InRoll</Link>

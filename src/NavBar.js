c5a5d2862569cd93665a8fb81188664185d092d1
regsah <123820415+regsah@users.noreply.github.com>
2024-07-19 22:03:34 +0300

    Updated NavBar.js to use Bootstrap

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-center bg-light container-fluid">
+      <div class="container-xl row justify-content-between">
+          <Link to="/jobs" class="nav-link col-3 col-md-2 p-1 fs-5 fw-bolder text-center">Job Board</Link>
+          <Link to="/" class="nav-link col-3 col-md-2 p-1 fs-4 fw-bold text-center">InRoll</Link>
+          <Link to="/Login" class="btn btn-outline-primary col-3 col-md-2 p-1 fs-5 fw-bolder rounded text-center">Login</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default NavBar;

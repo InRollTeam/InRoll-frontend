df722108228c10eb883384cd8a194736a08054e8
regsah <123820415+regsah@users.noreply.github.com>
2024-07-20 17:02:58 +0300

    Update App.js for supporting Login functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
 import NavBar from './NavBar';
 import InfoBar from './InfoBar';
 import Home from './Home';
 import JobBoard from './JobBoard';
+import Login from './Login';
 
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const handleCloseLogin = () => setShowLogin(false);
+
   return (
     <div className="App container-fluid bg-light py-1">
       <Router>
-        <NavBar />
+        <NavBar showLogin={showLogin} setShowLogin={setShowLogin} />
         <Switch>
           <Route exact path="/"> <Home /> </Route>
           <Route exact path="/Jobs"> <JobBoard /> </Route>
         </Switch>
+        <Login showLogin={showLogin} handleClose={handleCloseLogin} />
+        {showLogin ? <div class={`modal-backdrop fade ${showLogin ? 'show' : ''}`}></div> : null}
         <InfoBar />
       </Router>
     </div>

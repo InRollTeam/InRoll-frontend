49bffb5b5492396c8abfeb8beb1a642fbe7a11af
regsah <uregilsahin@gmail.com>
2024-07-23 14:34:03 +0300

    Implemented an InfoBar
    Added a bar for contact with placeholder information to the bottom of all pages.

--- src/Home.js
+++ src/Home.js
@@ -9,12 +9,13 @@ const Home = () => {
         <div class="card-body justify-content-start">
           <h5 class="card-title my-5 py-2 fs-1">Helping Companies Recruit Better</h5>
           <p class="card-text my-5 py-2 fs-5">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet,.</p>
-          <Link to="/demo" class="btn btn-outline-primary my-5 fs-5">Try our demo</Link>
+          <Link to="#" class="btn btn-outline-primary my-5 fs-5">Try our demo</Link>
         </div>
       </div>
       
       <img className="col-4 rounded-pill overflow-hidden m-5 d-none d-md-block" src={placeHolder} alt="Logo" />
     </div>
+    
   );
 }
  

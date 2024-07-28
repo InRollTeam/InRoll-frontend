2b9bcfaf6cbd33ac13e4f34a7758fb0e26ed29a5
regsah <uregilsahin@gmail.com>
2024-07-28 19:59:33 +0300

    Removed redundant test code

--- src/Test.js
+++ src/Test.js
@@ -44,22 +44,6 @@ const Test = () => {
   //alttaki satır geri eklenmeli (veya düzgün bir error sayfası ayarlanabilir)
   //if (error) return <p>Error: {error.message}</p>;
 
-
-
-/*
-  const {id} = useParams()
-  let data = {
-    "id"        : id,
-    "name"      : `Summarify-${id}`,
-    "body"      : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras arcu tellus, molestie ac scelerisque vitae, porta vitae lacus. Mauris bibendum tellus turpis, et mattis ligula molestie commodo. Cras tristique risus a volutpat bibendum. Aliquam tristique elit quis tellus blandit, vitae rutrum nisi mattis. Maecenas eget tellus justo. Nunc sit amet libero ac quam maximus posuere. Vestibulum eget nisi congue, commodo nisl a, congue magna. Ut interdum urna vel eros lobortis, vel interdum quam dictum. Aliquam imperdiet, tellus eu egestas aliquam, quam justo tempor leo, sed feugiat ligula felis ut nisi. Integer ac ullamcorper augue. Nam gravida ante at dolor vulputate, quis laoreet ipsum pulvinar. Fusce posuere aliquet justo. Curabitur elementum tempus leo, in fringilla mauris tincidunt ut. Maecenas vitae rutrum ipsum." +
-                  "\n\n" +
-                  "Praesent scelerisque id ex non ultrices. Nulla dignissim est ante, ac sollicitudin nibh dictum et. Curabitur ut tempus elit, a lacinia tortor. Duis accumsan dapibus purus vitae mollis. Praesent libero neque, auctor et efficitur et, commodo in ex. Integer suscipit ultricies lectus in dapibus. Phasellus lacinia mauris urna, a iaculis purus feugiat vel. Quisque facilisis fermentum faucibus. In sed venenatis nisi. Cras placerat risus est, in porttitor diam ultrices a. Duis accumsan, purus ut volutpat venenatis, urna elit pulvinar enim, nec congue augue dolor eget lorem. Mauris tincidunt mauris nulla, ut vehicula libero hendrerit id." +
-                  "\n\n" +
-                  "Aliquam sed eleifend libero. Praesent hendrerit, ex venenatis laoreet pellentesque, tortor tellus lacinia lectus, nec convallis sapien ex sed purus. Etiam sit amet quam lectus. Sed id venenatis enim. Donec at enim nunc. Morbi fermentum urna quam, id feugiat est sodales sed. Cras nec fermentum ante, tincidunt eleifend sapien. Nam orci ex, cursus nec tincidunt at, maximus quis augue. Nulla ultricies metus ac urna sagittis, malesuada aliquam velit porta. Etiam nec porta nunc, et varius justo.",
-    "time"      : 90,
-    "questions" : 75,
-  }
-*/
   return (
     <div className="Test container-fluid p-5 row d-flex flex-column justify-content-center">
 

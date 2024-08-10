7e1877d33f1e2355eed1ac5ded346af7ef751a71
regsah <uregilsahin@gmail.com>
2024-08-11 00:20:45 +0300

    Added the ability to create tests

---
+++ src/AddChoices.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+
+const AddChoices = ( {questions, setQuestions, index} ) => {
+
+  useEffect(()=>{
+    const updatedQuestions = [...questions];
+    if(!updatedQuestions[index].choices) updatedQuestions[index].choices = [];
+    setQuestions(updatedQuestions);
+  }, [])
+
+  const addChoice = () => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].choices.push({ body: "" });
+    setQuestions(updatedQuestions);
+  };
+
+  const deleteChoice = (choiceIndex) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].choices.splice(choiceIndex, 1);
+    setQuestions(updatedQuestions);
+  }
+
+  return (
+    <div className="AddChoices container-sm mt-3">
+
+      {
+        questions[index].choices ?
+        questions[index].choices.map((choice, choiceIndex) => (
+            <div key={choiceIndex} className="container-fluid">
+              <div className="container-md bg-light d-flex flew-row m-1">
+                <button className="btn btn-danger align-self-end m-1" type="button" onClick={() => deleteChoice(choiceIndex)}> <i class="bi bi-trash"></i> </button>
+                <textarea className="form-control m-0" id={`choice-body-${choiceIndex}`} rows={1}/>
+              </div>
+            </div>
+          )) :
+          null
+      }    
+  
+      <button type="button" className="btn text-secondary mt-3 fs-2 align-self-center" onClick={addChoice}><i class="bi bi-plus-square-fill"></i></button>
+    </div>
+  );
+}
+ 
+export default AddChoices;
---
+++ src/AddQuestions.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import AddChoices from "./AddChoices";
+
+const AddQuestions = ( {questions, setQuestions} ) => {
+  
+  const updateQuestion = (body, index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index] = { ...updatedQuestions[index], body: body };
+    setQuestions(updatedQuestions);
+  };
+
+  const addQuestion = () => {
+    const updatedQuestions = [...questions];
+    updatedQuestions.push({ body: "" });
+    setQuestions(updatedQuestions);
+  };
+
+  const deleteQuestion = (index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions.splice(index, 1);
+    setQuestions(updatedQuestions);
+  }
+
+
+  return (
+    <div className="AddQuestion container-md d-flex flex-column">
+      <div className="accordion">
+        {
+          questions.map((question, index) => (
+            <div key={index} className="accordion-item">
+              <h2 className="accordion-header" id={`heading-${index}`}>
+                <button 
+                  className="accordion-button bg-light-subtle" 
+                  type="button" 
+                  onClick={() => {
+                    const elem = document.getElementById(`collapse-${index}`);
+                    elem.classList.toggle("show");
+                  }}>
+                  question - {index}
+                </button>
+              </h2>
+
+              <div id={`collapse-${index}`} className="accordion-collapse collapse show">
+                <div className="accordion-body d-flex flex-column bg-light">
+
+                  <button className="btn btn-danger align-self-end m-0" type="button" onClick={() => deleteQuestion(index)}> <i class="bi bi-trash"></i> </button>
+
+                  <div className="container-md bg-light">
+                    <label htmlFor={`question-body-${index}`} className="form-label m-0"> Question </label>
+                    <textarea className="form-control m-0" id={`question-body-${index}`} rows="3" onChange={(e) => {updateQuestion(e.target.value, index)}}/>
+                  </div>
+                  
+                  {
+                    questions ? 
+                    <AddChoices questions={questions} setQuestions={setQuestions} index={index} /> :
+                    null
+                  }
+
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+      <button type="button" className="btn text-light fs-2 align-self-center" onClick={addQuestion}><i class="bi bi-plus-square-fill"></i></button>
+
+    </div>
+  );
+
+}
+ 
+export default AddQuestions;
